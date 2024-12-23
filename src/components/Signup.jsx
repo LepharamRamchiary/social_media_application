37012112e859cc2669115d9bf1f1d29b3a0d1438
LepharamRamchiary <lepharamchiary@gmail.com>
2024-12-23 21:52:05 +0530

    add signup

---
+++ src/components/Signup.jsx
@@ -0,0 +1,88 @@
+import authService from "../appwrite/auth";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import Button from "./Button";
+import Input from "./Input";
+import Logo from "./Logo";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { login } from "../store/authSlice.js";
+
+function Signup() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const create = async (data) => {
+    setError("");
+    try {
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        const currentUser = await authService.getCurrentUser();
+        if (currentUser) {
+          dispatch(login({ currentUser }));
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sing up to create anccount
+        </h2>
+
+        <p className="nt-2 text-center text-base text-black/60">
+          Already have an account ? &nbsp;
+          <Link
+            to="/login"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sign in
+          </Link>
+        </p>
+
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+        <form onSubmit={handleSubmit(create)} className="mt-8">
+          <Input
+            {...register("name", { required: true })}
+            label="Full name"
+            placeholder="Full name"
+          />
+          <Input
+            {...register("email", { required: true })}
+            label="Email"
+            placeholder="Email"
+            type="email"
+          />
+
+          <Input
+            {...register("password", { required: true })}
+            label="Password"
+            type="password"
+            placeholder="Password"
+          />
+
+          <Button type="submit" className="w-full">
+            Create Account
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
