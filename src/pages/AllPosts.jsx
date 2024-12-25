d70cbc7baef9fb7f8695a60fe940ad8ea94f3bf5
LepharamRamchiary <lepharamchiary@gmail.com>
2024-12-25 07:54:47 +0530

    add allpost page

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import appwriteService from "../appwrite/config";
+import Container from "../components/container/Container";
+import PostCard from "../components/PostCard";
 
 function AllPosts() {
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPost(posts.documents);
+      }
+    });
+  }, []);
   return (
-    <div>AllPosts</div>
-  )
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {post.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default AllPosts
+export default AllPosts;
